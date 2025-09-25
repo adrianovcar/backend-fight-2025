ffa1969efa45a3c257cde1ff0597414eb74006c3
Adriano Carrijo <adrianovieirac@gmail.com>
2025-09-25 07:48:17 -0300

    make post request working

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PaymentRequest;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Redis;
 
@@ -10,15 +12,52 @@ class PaymentsController extends Controller
 {
     /**
      * Store a newly created resource in storage.
+     * @throws ConnectionException
      */
-    public function payments(PaymentRequest $request): \Illuminate\Http\JsonResponse
+    public function payments(PaymentRequest $request)
     {
         $data = $request->validated();
-        // Store request into redis cache with prefix 'queue'
-        Redis::rpush('queue', json_encode($data));
+//        Redis::set('payment_processor', getenv('PAYMENT_PROCESSOR_URL'));
+//        Redis::set('payment_processor', getenv('PAYMENT_PROCESSOR_FALLBACK_URL'));
 
-        $response = Http::post(getenv('PAYMENT_PROCESSOR_URL'), $data);
+        $uuid = $request->input('correlationId');
+        $amount = $request->input('amount');
 
-        return response()->json($response->json(), $response->status());
+        try {
+            $response = Http::timeout(1)->connectTimeout(1)->post(Redis::get('payment_processor'), $data);
+
+            if ($response->successful()) {
+                self::callPipelining($uuid, $amount, 1);
+            } else {
+                $response = Http::timeout(1)->connectTimeout(1)->post(getenv('PAYMENT_PROCESSOR_FALLBACK_URL'), $data);
+
+                if ($response->successful()) {
+                    self::callPipelining($uuid, $amount, 2);
+                }
+            }
+
+            return response()->json(1);
+
+
+        } catch (ConnectionException | RequestException $e) {
+            return response()->json(['error' => 'Payment processor unavailable'], 503);
+        }
+
+        return response()->json(['error' => 'Payment processor unavailable'], 503);
+    }
+
+    protected static function callPipelining($uuid, $amount, $processor)
+    {
+        Redis::pipeline(function ($pipe) use ($uuid, $amount, $processor) {
+            $pipe->xadd('pay_success', '*', [
+                'uuid' => $uuid,
+                'amount' => $amount,
+                'processor' => $processor,
+                'ts' => now()->toIso8601String(),
+            ]);
+
+            $pipe->incr('pay_count');
+            $pipe->incrbyfloat('pay_amount_sum', $amount);
+        });
     }
 }
